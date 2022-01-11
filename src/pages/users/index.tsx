b9470f3945312dadd9c0777595618a666c17c45c
Gustavo Assuncao <gust.krv@gmail.com>
2022-01-10 21:41:07 -0300

    add hooks

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,36 +1,15 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner} from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
 
-    const {data, isLoading, error} = useQuery('users', async () => {
-       const response = await fetch('http://localhost:3000/api/users');
-       const data = await response.json()
-
-       const users = data.users.map(user => {
-           return {
-               id: user.id,
-               name: user.name,
-               email: user.email,
-               createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                   day: '2-digit',
-                   month:'long',
-                   year: 'numeric'
-               })
-           }
-       });
-
-       return users
-    }, {
-        staleTime: 1000 * 5,
-    })
+    const {data, isLoading, isFetching, error} = useUsers()
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -47,7 +26,10 @@ export default function UserList() {
                <Box flex="1" borderRadius={8} bg="gray.800" p="8" >
 
                    <Flex mb="8" justify="space-between" align="center" >
-                       <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                       <Heading size="lg" fontWeight="normal">
+                           Usuários
+                           { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/> }
+                           </Heading>
 
                         <Link href={"/users/create"} passHref>
                             <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>}>
